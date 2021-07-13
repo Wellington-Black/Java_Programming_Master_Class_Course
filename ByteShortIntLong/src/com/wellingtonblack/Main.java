d2fb395006979b443f2837f64f1ff0f76ac4b3f3
Wellington-Black <well1482@gmail.com>
2021-07-13 10:44:55 +0100

    Byte Short Int Long lesson

---
+++ ByteShortIntLong/src/com/wellingtonblack/Main.java
@@ -0,0 +1,43 @@
+package com.wellingtonblack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Int. Has a width of 32 bits.
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimun Value = " + myMinIntValue);
+        System.out.println("Integer Maximun Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1)); //Overflow
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1)); //Underflow
+
+        int myMaxIntTest = 2147483647;
+
+        //Byte. Occupies 8 bits. Width of 8.
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimun Value = " + myMinByteValue);
+        System.out.println("Byte Maximun Value = " + myMaxByteValue);
+
+        //Short. Width of 16 bits.
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimun Value = " + myMinShortValue);
+        System.out.println("Short Maximun Value = " + myMaxShortValue);
+
+        //Long. Width of 64.
+
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimun Value = " + myMinLongValue);
+        System.out.println("Long Maximun Value = " + myMaxLongValue);
+    }
+}
